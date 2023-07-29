52d0a03fe3f8dc84654830f2f419ad023322c6b2
Pawel Maliszewski <pablo999455@gmail.com>
2023-07-29 19:22:10 +0200

    start

---
+++ src/main/java/com/example/temat21zad1/Temat21zad1Application.java
@@ -0,0 +1,13 @@
+package com.example.temat21zad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Temat21zad1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Temat21zad1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/temat21zad1/Temat21zad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.temat21zad1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Temat21zad1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
